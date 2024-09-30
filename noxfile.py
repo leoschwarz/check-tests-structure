0ede244846382a3b5ab2f570c484c20fde902ade
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-30 15:47:50 +0200

    add licensecheck

--- noxfile.py
+++ noxfile.py
@@ -13,3 +13,10 @@ def try_hook(session) -> None:
     """Tests the pre-commit hook in this repo."""
     session.install("pre-commit")
     session.run("pre-commit", "try-repo", ".", "--verbose")
+
+
+@nox.session
+def licensecheck(session) -> None:
+    """Runs the license check."""
+    session.install("licensecheck")
+    session.run("licensecheck")

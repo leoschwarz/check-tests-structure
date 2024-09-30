757adbf686263293904770b0a405848f4b46c927
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-30 15:21:37 +0200

    improve setup

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,10 @@ def tests(session) -> None:
     """Runs the test suite."""
     session.install(".[test]")
     session.run("pytest")
+
+
+@nox.session
+def try_hook(session) -> None:
+    """Tests the pre-commit hook in this repo."""
+    session.install("pre-commit")
+    session.run("pre-commit", "try-repo", ".", "--verbose")

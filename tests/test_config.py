8965d10d3be4eb4802a248531e83a117836d2583
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-30 14:51:57 +0200

    add test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from check_tests_structure.config import find_pyproject_toml
+from check_tests_structure.config import find_pyproject_toml, parse_pyproject_toml
 
 
 def test_find_pyproject_toml_initial_when_project_root(fs):
@@ -25,3 +25,15 @@ def test_find_pyproject_toml_when_sub_sub_directory(fs):
 
 def test_find_pyproject_toml_when_not_found(fs):
     assert find_pyproject_toml(Path("/my_project")) is None
+
+
+def test_parse_pyproject_toml_when_minimal(mocker):
+    mock_path = mocker.Mock()
+    mock_path.read_text.return_value = """
+    [tool.check-tests-structure]
+    sources_path = "src"
+    tests_path = "tests"
+    """
+    config = parse_pyproject_toml(path=mock_path)
+    assert config.sources_path == Path("src")
+    assert config.tests_path == Path("tests")
